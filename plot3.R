532b707afad709544e836883c9cd43da3897a2aa
BigDataNerd <bdn@kapson.net>
2014-05-09 09:03:40 -0400

    Added code to ignore NA values in data during plotting.

--- plot3.R
+++ plot3.R
@@ -18,8 +18,8 @@ data <- alldata[alldata$datetime >= as.POSIXlt("2007-02-01") & alldata$datetime 
 
 # make plot3 using png device
 png("plot3.png", width=480, height=480, units="px", bg="transparent")
-with(data=data, plot(datetime,Sub_metering_1, main="", ylab='Energy sub metering', type='l',xlab=""))
-with(data=data, lines(datetime, Sub_metering_2, col='red'))
-with(data=data, lines(datetime, Sub_metering_3, col='blue'))
+with(data=data[!is.na(data$Sub_metering_1),], plot(datetime,Sub_metering_1, main="", ylab='Energy sub metering', type='l',xlab=""))
+with(data=data[!is.na(data$Sub_metering_2),], lines(datetime, Sub_metering_2, col='red'))
+with(data=data[!is.na(data$Sub_metering_3),], lines(datetime, Sub_metering_3, col='blue'))
 legend("topright",c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),lwd=2,col=c('black','red','blue'))
 dev.off()
